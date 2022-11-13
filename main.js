6b50e83f929d9a892a2d2ec2694ca45d2b1b9148
Bigkid6999 <J.fields@share.epsb.ca>
2022-11-12 18:52:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 //SIMPLE ADDITION IPO CALCULATOR BY MR.V
 
 //Input
-alert("hello")
+let Grade1 = prompt()
+let Grade2 = prompt()
+let Grade3 = prompt()
+let Grade4 = prompt()
+let Grade5 = prompt()
 //Process
 
 //Output
