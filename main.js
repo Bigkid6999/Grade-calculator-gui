c40f83129f3a06b7038712dc1fbb91c1cfc2ef50
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2022-12-21 14:05:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 //SIMPLE ADDITION IPO CALCULATOR BY MR.V
 
 //Input
-let 
-
+let grade1 = prompt("Enter the grade for client side scripting")
+let grade2 = prompt("Enter the grade for structured programing 1")
+let grade3 = prompt("Enter the grade for structured programing 2")
+let grade4 = prompt("Enter the grade for client side scripting 2")
+let grade5 = prompt("Enter the grade for project A")
 //Process
+let total = grade1 + grade2 + grade3 + grade4 + grade5 / 5
 
 
 //Output
