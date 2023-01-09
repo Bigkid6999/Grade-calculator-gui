dc7f8840446d998bf7cc8d5630d26cfa23138635
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2023-01-09 13:47:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 //SIMPLE ADDITION IPO CALCULATOR BY MR.V
-
+document.getElementById("btn").addEventListener("click", btnClicked)
 //Input
+
+function btnClicked(){
 let grade1 = +prompt("Enter the grade for client side scripting");
 let grade2 = +prompt("Enter the grade for structured programing 1");
 let grade3 = +prompt("Enter the grade for structured programing 2")
 let grade4 = +prompt("Enter the grade for client side scripting 2");
 let grade5 = +prompt("Enter the grade for project A");
- 
-//Process
+
 let total = grade1 + grade2 + grade3 + grade4 + grade5 /5;
 
+alert(total)
+}
+ 
 
-//Output
-alert$(total);
+
+prompt ("balls")
+
