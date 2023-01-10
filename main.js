8ecb1359b406bc73a78872b5ba68667e375dfe9c
Bigkid6999 <J.fields@share.epsb.ca>
2023-01-09 19:56:46 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //SIMPLE ADDITION IPO CALCULATOR BY MR.V
-
+document.getElementById("btn").addEventListener("click", btnClicked);
 //Input
+<<<<<<< HEAD
 <<<<<<< HEAD
 let Grade1 = prompt()
 let Grade2 = prompt()
@@ -12,6 +13,24 @@ let
 
 >>>>>>> 660259ebc74f9599e81a8e93dbcde735fff482df
 //Process
+=======
+>>>>>>> 1c368bc7f794ab7db05c505970742396995358cc
 
 
-//Output
+function btnClicked(){
+let grade1 = document.getElementById("1").value;
+let grade2 = document.getElementById("2").value;
+let grade3 = document.getElementById("3").value;
+let grade4 = document.getElementById("4").value;
+let grade5 = document.getElementById("5").value;
+
+let = total = document.getElementById("1")+document.getElementById ("2")
+
+
+ //Output
+alert(total);
+}
+ 
+
+
+
