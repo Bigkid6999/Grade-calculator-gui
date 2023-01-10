97e25b244a514560194fe8ed254659a31ec80772
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2023-01-10 14:22:21 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //SIMPLE ADDITION IPO CALCULATOR BY MR.V
 document.getElementById("btn").addEventListener("click", btnClicked);
 
+
 let grade1 = document.getElementbyId("1").value;
 let grade2 = document.getElementById("2").value;
 let grade3 = document.getElementById("3").value;
@@ -8,8 +9,8 @@ let grade4 = document.getElementById("4").value;
 let grade5 = document.getElementById("5").value;
 
 
-function btnClicked(){
-let total =  (grade1 + grade2 + grade3 + grade4 + grade5) /5
+function btnClicked () {
+let total = (grade1 + grade2 + grade3 + grade4 + grade5) /5
 
 
 alert(total);
