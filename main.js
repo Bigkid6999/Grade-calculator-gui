d07129bf944cceaf7ad10111d4fe87c0bf55ed17
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2023-01-11 13:53:44 -0700

    Revert "Update main.js"
    This reverts commit 1f80a2ee4598a5ad15014a0985404bd9f7ed0312.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 
-let grade1 = document.getElementById("1").value;
+let grade1 = document.getElementbyId("1").value;
 let grade2 = document.getElementById("2").value;
 let grade3 = document.getElementById("3").value;
 let grade4 = document.getElementById("4").value;
